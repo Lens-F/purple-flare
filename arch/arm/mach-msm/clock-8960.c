c494e4d7b9af4155d9c7dfbaec70626c89aca503
Lens-F <tnoah12@gmail.com>
2013-08-05 17:15:32 -0400

    initial gpu oc
    Conflicts:
    	arch/arm/mach-msm/board-m7-gpu.c
    	include/linux/msm_kgsl.h

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3307,6 +3307,8 @@ static struct clk_freq_tbl clk_tbl_gfx2d[] = {
 	F_GFX2D(177778000, pll2, 2,  9),
 	F_GFX2D(200000000, pll2, 1,  4),
 	F_GFX2D(228571000, pll2, 2,  7),
+	F_GFX2D(266667000, pll2, 1,  4),
+	F_GFX2D(320000000, pll2, 1,  3), 
 	F_END
 };
 
@@ -3349,8 +3351,8 @@ static struct rcg_clk gfx2d0_clk = {
 		.dbg_name = "gfx2d0_clk",
 		.ops = &clk_ops_rcg,
 		.flags = CLKFLAG_SKIP_HANDOFF,
-		VDD_DIG_FMAX_MAP3(LOW,  100000000, NOMINAL, 200000000,
-				  HIGH, 228571000),
+		VDD_DIG_FMAX_MAP3(LOW,  100000000, NOMINAL, 266667000,
+				  HIGH, 320000000),
 		CLK_INIT(gfx2d0_clk.c),
 	},
 };
@@ -3394,8 +3396,8 @@ static struct rcg_clk gfx2d1_clk = {
 		.dbg_name = "gfx2d1_clk",
 		.ops = &clk_ops_rcg,
 		.flags = CLKFLAG_SKIP_HANDOFF,
-		VDD_DIG_FMAX_MAP3(LOW,  100000000, NOMINAL, 200000000,
-				  HIGH, 228571000),
+		VDD_DIG_FMAX_MAP3(LOW,  100000000, NOMINAL, 266667000,
+				  HIGH, 320000000),
 		CLK_INIT(gfx2d1_clk.c),
 	},
 };
@@ -3428,6 +3430,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
 	F_GFX3D(450000000, pll15, 1,  2),
+	F_GFX3D(512000000, pll2,  2,  5),
 	F_END
 };
 
@@ -3455,19 +3458,19 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 static unsigned long fmax_gfx3d_8064ab[MAX_VDD_LEVELS] __initdata = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-	[VDD_DIG_HIGH]    = 450000000
+	[VDD_DIG_HIGH]    = 512000000
 };
 
 static unsigned long fmax_gfx3d_8064[MAX_VDD_LEVELS] __initdata = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-	[VDD_DIG_HIGH]    = 400000000
+	[VDD_DIG_HIGH]    = 512000000
 };
 
 static unsigned long fmax_gfx3d_8930[MAX_VDD_LEVELS] __initdata = {
 	[VDD_DIG_LOW]     = 192000000,
 	[VDD_DIG_NOMINAL] = 320000000,
-	[VDD_DIG_HIGH]    = 450000000
+	[VDD_DIG_HIGH]    = 512000000
 };
 
 static struct bank_masks bmnd_info_gfx3d = {
